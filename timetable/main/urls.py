9249fb7b168e59f18a9076988ea7841061568d3f
olshanskyvv <90987935+olshanskyvv@users.noreply.github.com>
2022-10-23 20:40:07 +0300

    Delete separate auth and start apps

--- timetable/main/urls.py
+++ timetable/main/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('timetable', views.timetable, name='timetable')
+    path('', views.index, name='home'),
+    path('timetable', views.timetable, name='timetable'),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
 ]
--- timetable/main/views.py
+++ timetable/main/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from .code.searcher import find_group_url
 from .code.parser import parse
 
 
+def index(request):
+    return HttpResponse('<h1>Стартовая страница</h1>')
+
+
 def timetable(request):
     table = parse(find_group_url('УИС-212'))
     return render(request, 'main/timetable.html', {'table': table})
+
+
+def login(request):
+    return HttpResponse('<h1>Страница с входом</h1>')
+
+
+def register(request):
+    return HttpResponse('<h1>Страница с регистрацией</h1>')
