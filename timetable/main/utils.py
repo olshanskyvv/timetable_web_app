165293635e66e776d1bc98401e1b563ffe32f47e
olshanskyvv <90987935+olshanskyvv@users.noreply.github.com>
2022-10-25 23:13:40 +0300

    Add menu list to maxin

---
+++ timetable/main/utils.py
@@ -0,0 +1,13 @@
+menu = [
+    {'title': 'Главная', 'url_name': 'home'},
+    {'title': 'Рассписание', 'url_name': 'timetable'},
+    {'title': 'Вход', 'url_name': 'login'},
+    {'title': 'Регистрация', 'url_name': 'register'},
+]
+
+
+class DataMixin:
+    def get_user_context(self, **kwargs):
+        context = kwargs
+        context['menu'] = menu
+        return context
--- timetable/main/views.py
+++ timetable/main/views.py
@@ -4,28 +4,42 @@ from django.views.generic import TemplateView
 
 from .code.searcher import find_group_url
 from .code.parser import parse
+from .utils import DataMixin
 
 
-# def index(request):
-#     return render(request, 'main/index.html')
-
-
-class IndexView(TemplateView):
+class IndexView(DataMixin, TemplateView):
     template_name = 'main/index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Главная страница')
+        return dict(list(context.items()) + list(c_def.items()))
 
-class TimetableView(TemplateView):
+
+class TimetableView(DataMixin, TemplateView):
     template_name = 'main/timetable.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['table'] = parse(find_group_url('УИС-212'))
-        return context
+        table = parse(find_group_url('УИС-212'))
+        c_def = self.get_user_context(title='Расписание',
+                                      table=table)
+        return dict(list(context.items()) + list(c_def.items()))
 
 
-class LoginView(TemplateView):
+class LoginView(DataMixin, TemplateView):
     template_name = 'main/login.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Вход')
+        return dict(list(context.items()) + list(c_def.items()))
 
-class RegisterView(TemplateView):
+
+class RegisterView(DataMixin, TemplateView):
     template_name = 'main/register.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Регистрация')
+        return dict(list(context.items()) + list(c_def.items()))
