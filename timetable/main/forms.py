6ee55f57bcac31287c2e33d11dc7584efcfaeadb
olshanskyvv <90987935+olshanskyvv@users.noreply.github.com>
2022-10-26 12:46:19 +0300

    Add validation for group field

--- timetable/main/forms.py
+++ timetable/main/forms.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django import forms
+from re import match
 
 from .models import *
+
+
+def group_is_valid(value):
+    if not match(r'\w{3}-\d{3}', value):
+        raise forms.ValidationError("Группа не соответствует формату: AAA-111")
 
 
 class RegisterUserForm(UserCreationForm):
@@ -9,7 +15,7 @@ class RegisterUserForm(UserCreationForm):
     first_name = forms.CharField(label='Имя', widget=forms.TextInput(attrs={'class': 'form-input'}))
     last_name = forms.CharField(label='Фамилия', widget=forms.TextInput(attrs={'class': 'form-input'}))
     email = forms.CharField(label='Email', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    group = forms.CharField(label='Номер группы', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    group = forms.CharField(label='Номер группы', validators=[group_is_valid], widget=forms.TextInput(attrs={'class': 'form-input'}))
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
     password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
 
