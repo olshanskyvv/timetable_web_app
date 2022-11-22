3eaf11b8a4640fb498608108de44ceaa653c9bb4
olshanskyvv <90987935+olshanskyvv@users.noreply.github.com>
2022-11-22 18:46:12 +0300

    Fix email field displaying on the registration page and username displaying at the admin panel

--- timetable/main/admin.py
+++ timetable/main/admin.py
@@ -5,9 +5,9 @@ from .models import User
 
 
 class MyUserAdmin(UserAdmin):
-    list_display = ('last_name', 'first_name', 'group', 'email', 'is_elder')
-    list_display_links = ('last_name', 'first_name')
-    search_fields = ('last_name', 'first_name')
+    list_display = ('username', 'last_name', 'first_name', 'group', 'email', 'is_elder')
+    list_display_links = ('username', 'last_name', 'first_name')
+    search_fields = ('username', 'last_name', 'first_name')
     list_editable = ('is_elder',)
 
 
